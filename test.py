55fb8867b3ae6c0723beb1e7badc7338c9207295
Waterstone <760305100@qq.com>
2020-07-22 17:26:37 +0800

    根据输入的名称和分卷个数n创建Vol.n_SUB文件夹,选择BDMV分卷源地址后自动填充相应文件至Vol.n_SUB

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from tkinter import filedialog
 root = tk.Tk()
 
 SUB_Dir = "F:/ACG/Animate/[BDMV] 加流重灌/"
-BDMV_Dir = "F:\ACG\Animate\[BDMV]"
+BDMV_Dir = "F:/ACG/Animate/[BDMV]"
 
 print(u'测试开始')
 
@@ -19,14 +19,17 @@ for i in range(1, int(vol_num)+1):
 print("成功创建目录")
 
 
-var1 = 'waterstone977.ddns.net'
-#var_source = '"F:\ACG\Animate\[BDMV]\[BDMV][アニメ][虚构推理][Kyokou Suiri][虚構推理][Vol.1-Vol.4 Fin][JP]\SUIRI_4\BDROM"'
-#var_target = '"F:\ACG\Animate\[BDMV] 加流重灌\虚构推理\Vol.4"'
-var_source = filedialog.askdirectory()
-#print(var_source)
-var_target = filedialog.askdirectory()
-#print(var_target)
+var_source = []
+for i in range(1, int(vol_num)+1):
+    var_source.append(filedialog.askdirectory())
+
+var_target = []
+for i in range(1, int(vol_num)+1):
+    var_target.append(SUB_Dir + bdmv_name + "/Vol." + str(i) + "_SUB")
+
 #print('echo d | xcopy "' + var_source + '" "' + var_target + '" /s /e /exclude:uncopy.txt')
-os.system('echo d | xcopy "' + var_source + '" "' + var_target + '" /s /e /exclude:uncopy.txt')
+for i in range (0, int(vol_num)):
+    os.system('echo d | xcopy "' + var_source[i] + '" "' + var_target[i] + '" /s /e /exclude:uncopy.txt')
+
 os.system('pause')
 print(u'测试结束')
